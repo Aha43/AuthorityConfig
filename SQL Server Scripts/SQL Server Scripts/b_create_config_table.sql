6180297b6b22dc49ac2651c7a5e5f4029b9a6b27
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-05 20:19:22 +0100

    sp to create / update authority config and added missing not null on pk cross sql idiom

--- SQL Server Scripts/SQL Server Scripts/b_create_config_table.sql
+++ SQL Server Scripts/SQL Server Scripts/b_create_config_table.sql
@@ -1,5 +1,5 @@
 create table authconfig.config (
-	authority nvarchar(32) primary key,
+	authority nvarchar(32) not null primary key,
 	uri nvarchar(256) not null,
 	json nvarchar(max) not null,
 	description nvarchar(256) null
