0ad2110e5196c7531cca3922905da97c2736d42f
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-06 13:54:00 +0100

    Missing ioc configuration

---
+++ AuthorityConfig.Infrastructure.AuthorityRepository.Sql/Config/IoCConfig.cs
@@ -0,0 +1,17 @@
+﻿using AuthorityConfig.Specification.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorityConfig.Infrastructure.AuthorityRepository.Sql.Config
+{
+    public static class IoCConfig
+    {
+        public static IServiceCollection ConfigureSqlAuthorityRepository(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            return serviceCollection.AddSingleton(new DbConnectionProvider(configuration))
+                .AddSingleton<IAuthorityRepository, AuthorityRepository>();
+        }
+
+    }
+
+}
