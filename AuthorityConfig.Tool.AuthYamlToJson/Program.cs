4a6f6be6197b8e288c11ec8df324c7b0489e6482
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-06 14:29:42 +0100

    added new Project Tool.AuthYamlToJson

---
+++ AuthorityConfig.Tool.AuthYamlToJson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthorityConfig.Tool.AuthYamlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
