7f6b71d21b85d963306ad598fde6c16da4a5a537
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-06 15:26:27 +0100

    #26

--- AuthorityConfig.Tool.AuthYamlToJson/Program.cs
+++ AuthorityConfig.Tool.AuthYamlToJson/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using AuthorityConfig.Domain.Model;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text.Json;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NodeDeserializers;
 
 namespace AuthorityConfig.Tool.AuthYamlToJson
 {
@@ -6,7 +13,59 @@ namespace AuthorityConfig.Tool.AuthYamlToJson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var yamlPath = args[0];
+
+            // Read yaml
+            var config = LoadFromYaml(yamlPath);
+
+            // Generate json
+            var json = GenerateJson(config);
+
+            // Write json
+            Console.Out.Write(json);
         }
+
+        private static string GenerateJson(IdserverConfig conf)
+        {
+            string json = JsonSerializer.Serialize(conf, new JsonSerializerOptions { 
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            });
+            return json;
+        }
+
+        private static IdserverConfig LoadFromYaml(string path)
+        {
+            var deserializer = new DeserializerBuilder()
+                 .WithNodeDeserializer(inner => new ValidatingNodeDeserializer(inner), s => s.InsteadOf<ObjectNodeDeserializer>())
+                 .Build();
+
+            var yf = File.OpenText(path);
+            var idConf = deserializer.Deserialize<IdserverConfig>(yf);
+
+            return idConf;
+        }
+
+        public class ValidatingNodeDeserializer : INodeDeserializer
+        {
+            private readonly INodeDeserializer _nodeDeserializer;
+
+            public ValidatingNodeDeserializer(INodeDeserializer nodeDeserializer)
+            {
+                _nodeDeserializer = nodeDeserializer;
+            }
+
+            public bool Deserialize(IParser parser, Type expectedType, Func<IParser, Type, object> nestedObjectDeserializer, out object value)
+            {
+                if (!_nodeDeserializer.Deserialize(parser, expectedType, nestedObjectDeserializer, out value))
+                    return false;
+
+                var context = new ValidationContext(value, null, null);
+                Validator.ValidateObject(value, context, true);
+                return true;
+            }
+        }
+
     }
+
 }
