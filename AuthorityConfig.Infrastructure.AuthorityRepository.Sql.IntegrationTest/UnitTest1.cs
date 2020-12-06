e38d945cb75d53a21903e096ffc1a5b3e2ae94d5
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-06 13:56:12 +0100

    added new Project Infrastructure.AuthorityRepository.Sql.IntegrationTest

---
+++ AuthorityConfig.Infrastructure.AuthorityRepository.Sql.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AuthorityConfig.Infrastructure.AuthorityRepository.Sql.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
