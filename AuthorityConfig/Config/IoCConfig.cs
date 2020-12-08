953fb8489f983a9ecdd161c89e70266b8faf0afa
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-08 23:29:12 +0100

    Configuration of application services

---
+++ AuthorityConfig/Config/IoCConfig.cs
@@ -0,0 +1,18 @@
+﻿using AuthorityConfig.Infrastructure.AuthorityRepository.Sql.Config;
+using AuthorityConfig.Infrastructure.Manager.Config;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorityConfig.Config
+{
+    public static class IoCConfig
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.ConfigureSqlAuthorityRepository(configuration)
+                .ConfigureManagerServices();
+        }
+
+    }
+
+}
