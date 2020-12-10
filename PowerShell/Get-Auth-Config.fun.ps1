3191ad414e43c54768fb2ed5d62252a0772753be
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-11 00:32:17 +0100

    ps1 documentation

--- PowerShell/Get-Auth-Config.fun.ps1
+++ PowerShell/Get-Auth-Config.fun.ps1
@@ -1,5 +1,12 @@
 . (Join-Path -Path ".\tools" -ChildPath ".\Invoke-AuthorityConfig-Api.fun.ps1")
 
+<#
+.SYNOPSIS
+    Gets complete configuration of an authority
+.EXAMPLE
+    PS> Get-Auth-Config -Authority <authority>
+    Gets complete configuration of named authority
+#>
 function Get-Auth-Config(
     [Parameter(Mandatory = $true)][string]$Authority,
     [string]$AuthorityConfigAddress = $Env:AuthorityConfigAddress
