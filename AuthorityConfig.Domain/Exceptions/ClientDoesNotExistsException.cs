2029fdafd2f404ffd65048d8dceb1025eb680493
Arne Halvorsen <arne.halvorsen@gmail.com>
2020-12-27 17:38:20 +0100

    ClientExistsException renamed to ClientDoesNotExistsException and now accept ClientId

---
+++ AuthorityConfig.Domain/Exceptions/ClientDoesNotExistsException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AuthorityConfig.Domain.Exceptions
+{
+    public class ClientDoesNotExistsException : Exception
+    {
+        public ClientDoesNotExistsException(string clientId) : base("Client with id: '" + clientId + "' does not exists") { }
+    }
+}
--- AuthorityConfig.Domain/Exceptions/ClientExistsException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AuthorityConfig.Domain.Exceptions
-{
-    public class ClientExistsException : Exception
-    {
-        public ClientExistsException(string message) : base(message) { }
-    }
-}
